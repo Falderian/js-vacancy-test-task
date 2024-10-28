146679316cb4bb4004fa96bc094952dfac6b091c
Nikita <n.staselovich@codemaker.click>
2024-10-28 22:23:40 +0300

    Fixed the missing syntax

--- apps/api/src/resources/account/actions/get.ts
+++ apps/api/src/resources/account/actions/get.ts
@@ -6,7 +6,7 @@ async function handler(ctx: AppKoaContext) {
   console.log('ctx from actions.get', ctx);
   console.log('ctx.state.user', ctx.state.user);
   console.log('userService.getPublic(ctx.state.user)', userService.getPublic(ctx.state.user));
-  console.log('isShadow: ctx.state.isShadow,', isShadow: ctx.state.isShadow,)
+  console.log('isShadow: ctx.state.isShadow,', { isShadow: ctx.state.isShadow });
   ctx.body = {
     ...userService.getPublic(ctx.state.user),
     isShadow: ctx.state.isShadow,
